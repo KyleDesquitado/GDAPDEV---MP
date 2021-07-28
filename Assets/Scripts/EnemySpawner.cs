2a554743d9a5959344d918b9af1163da266740a1
Kyle Francis Desquitado <kyle_francis_desquitado@dlsu.edu.ph>
2021-07-29 00:48:11 +0800

    added enemy spawner script

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] List<GameObject> enemySpawnList;
+    [SerializeField] GameObject enemyObj;
+    [SerializeField] GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        while(enemySpawnList.Count != 0)
+        {
+            GameObject enemySpawn = GameObject.Instantiate(enemyObj, enemySpawnList[0].transform.position, Quaternion.identity, null);
+            enemySpawn.transform.LookAt(player.transform);
+            enemySpawnList.RemoveAt(0);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

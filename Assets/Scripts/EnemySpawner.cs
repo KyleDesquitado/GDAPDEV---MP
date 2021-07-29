9963fb8a77154c965f3b1711517016434203c60d
Kyle Francis Desquitado <kyle_francis_desquitado@dlsu.edu.ph>
2021-07-30 01:02:24 +0800

    continuous enemy spawn, enemy attack

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -11,12 +11,20 @@ public class EnemySpawner : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        while(enemySpawnList.Count != 0)
+        //while(enemySpawnList.Count != 0)
+        //{
+        //    GameObject enemySpawn = GameObject.Instantiate(enemyObj, enemySpawnList[0].transform.position, Quaternion.identity, null);
+        //    enemySpawn.transform.LookAt(player.transform);
+        //    targetHolder.addTarget(enemySpawn);
+        //    enemySpawnList.RemoveAt(0);
+        //}
+
+        for(int i = 0; i < enemySpawnList.Count; i++)
         {
-            GameObject enemySpawn = GameObject.Instantiate(enemyObj, enemySpawnList[0].transform.position, Quaternion.identity, null);
+            GameObject enemySpawn = GameObject.Instantiate(enemyObj, enemySpawnList[i].transform.position, Quaternion.identity, null);
             enemySpawn.transform.LookAt(player.transform);
+            enemySpawn.GetComponent<EnemyBehavior>().setTarget(player);
             targetHolder.addTarget(enemySpawn);
-            enemySpawnList.RemoveAt(0);
         }
     }
 
@@ -24,5 +32,32 @@ public class EnemySpawner : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public List<GameObject> getSpawnList()
+    {
+        return enemySpawnList;
+    }
+    
+    public void respawnEnemy(GameObject deadEnemy)
+    {
+        for(int i = 0; i < enemySpawnList.Count; i++)
+        {
+            if(deadEnemy.transform.position == enemySpawnList[i].transform.position)
+            {
+                float delayTime = Random.Range(7f, 15f);
+                StartCoroutine(reviveDelay(delayTime, i));
+            }
+        }
+
+    }
+
+    private IEnumerator reviveDelay(float waitTime, int index)
+    {
+        yield return new WaitForSeconds(waitTime);
+        GameObject enemySpawn = GameObject.Instantiate(enemyObj, enemySpawnList[index].transform.position, Quaternion.identity, null);
+        enemySpawn.transform.LookAt(player.transform);
+        enemySpawn.GetComponent<EnemyBehavior>().setTarget(player);
+        targetHolder.addTarget(enemySpawn);
     }
 }

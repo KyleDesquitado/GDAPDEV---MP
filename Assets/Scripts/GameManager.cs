0ea9086d0cbad47f787b7c38139d80326ce65a08
Setiel B. Olivarez <setiel_b_olivarez@dlsu.edu.ph>
2021-08-01 18:22:46 +0800

    manager
    (cherry picked from commit 354e040aadc029f6c3a3120b966393d5114c7fa0)

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,10 +4,26 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+
+    public static GameManager Instance;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
     [SerializeField] HandheldCameraBehavior camera;
     [SerializeField] PlayerController player;
     [SerializeField] EnemySpawner spawner;
-    GameObject target;
+    GameObject enemyFound;
+
+    public List<GameObject> enemyList;
 
     // Start is called before the first frame update
     void Start()
@@ -20,13 +36,39 @@ public class GameManager : MonoBehaviour
     {
         if(player.getCameraFlashBool())
         {
-            target = camera.getVisibleTarget();
-            if(target != null)
+            enemyFound = camera.getVisibleTarget();
+            if(enemyFound != null)
             {
-                spawner.respawnEnemy(target);
-                target.SetActive(false);
-                camera.removeTarget(target);
+                enemyFound.GetComponent<EnemyStats>().displayStats();
+                OnHit(enemyFound);
+                //spawner.respawnEnemy(target);
+                //target.SetActive(false);
+                //camera.removeTarget(target);
             }
         }
     }
+
+
+    public void OnHit(GameObject enemy)
+    {
+        // if conditions for enemy ids corresponding damage
+        enemy.GetComponent<EnemyStats>().stats.HP -= 1;
+
+        if (enemy.GetComponent<EnemyStats>().stats.HP ==0)
+        {
+            enemy.SetActive(false);
+            removeFromEnemyList(enemy);
+        }
+    }
+
+    public void addToEnemyList(GameObject enemy)
+    {
+        enemyList.Add(enemy);
+    }
+
+    public void removeFromEnemyList(GameObject enemy)
+    {
+        enemyList.Remove(enemy);
+    }
 }
+
